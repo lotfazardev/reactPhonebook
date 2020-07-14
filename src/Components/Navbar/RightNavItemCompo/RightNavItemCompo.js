70aa272f3bddcab4b41b608ed28d631bb94fbfc1
Mohammad H. Lotfazar <lotfazardev@users.noreply.github.com>
2020-07-14 14:14:48 +0430

    adjust space in leftnav

--- src/Components/Navbar/RightNavItemCompo/RightNavItemCompo.js
+++ src/Components/Navbar/RightNavItemCompo/RightNavItemCompo.js
@@ -4,8 +4,8 @@ import './RightNavItemCompo.scss'
 function RightLeftNavItems(props) {
     let tmp = props.selected ? "nav-item-selected" : "";
     return (
-        <Col className={"no-padding d-flex align-items-center nav-item col-12 "+ tmp }>
-            <Col className="no-padding col-12 col-lg-4 nav-item d-flex justify-content-center align-items-center">
+        <Col className={"no-padding d-flex justify-content-end align-items-center nav-item col-12 "+ tmp }>
+            <Col className="no-padding col-12 col-lg-3 nav-item d-flex justify-content-center align-items-center">
                 <span className={props.navItemIcon + " navItemIcon"}></span>
             </Col>
             <Col className="no-padding d-none d-md-flex col-lg-8">
