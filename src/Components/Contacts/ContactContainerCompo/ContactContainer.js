d704699e1a12d726f3778e7fe4479dc6e5c0820c
Mohammad H. Lotfazar <lotfazardev@users.noreply.github.com>
2020-07-15 20:53:11 +0430

    delete Contact title for better ux

--- src/Components/Contacts/ContactContainerCompo/ContactContainer.js
+++ src/Components/Contacts/ContactContainerCompo/ContactContainer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ContactTitle from '../ContactTitleCompo/ContactTitle';
 import './ContactContainer.scss';
 import ContactButton from '../ContactButtonCompo/ContactButton'
 import { Col } from 'react-bootstrap';
@@ -8,10 +7,7 @@ import ContactSreachbar from '../ContactSearchbarCompo/ContactSearchbar';
 function ContactContainer(props) {
     return (
         <>
-            <Col className="no-padding col-12">
-                <ContactTitle title="لیست مخاطبین" />
-            </Col>
-            <Col className="no-padding col-12 d-flex justify-content-between align-items-center">
+            <Col className="no-padding col-12 d-flex justify-content-between align-items-center mt-1">
                 <ContactButton buttonName="افزودن مخاطب" buttonIcon="icon-user-plus" buttonColor="#5DA2D5" />
                 <ContactSreachbar/>
             </Col>
