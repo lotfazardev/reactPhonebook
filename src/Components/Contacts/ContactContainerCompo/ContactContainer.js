bd720248e7f0465d31e69c655cc81a2680fd0178
Mohammad H. Lotfazar <lotfazardev@users.noreply.github.com>
2020-07-15 15:25:29 +0430

    move col div from ContactTitle to ContactContainer

--- src/Components/Contacts/ContactContainerCompo/ContactContainer.js
+++ src/Components/Contacts/ContactContainerCompo/ContactContainer.js
@@ -7,9 +7,11 @@ import { Col } from 'react-bootstrap';
 function ContactContainer(props) {
     return (
         <>
-            <ContactTitle title="لیست مخاطبین"/>
+            <Col className="no-padding col-12">
+                <ContactTitle title="لیست مخاطبین" />
+            </Col>
             <Col className="no-padding col-12 d-flex justify-content-between align-items-center">
-                <ContactButton buttonName="افزودن مخاطب" buttonIcon="icon-user-plus" buttonColor="#5DA2D5"/>
+                <ContactButton buttonName="افزودن مخاطب" buttonIcon="icon-user-plus" buttonColor="#5DA2D5" />
             </Col>
         </>
     );
--- src/Components/Contacts/ContactTitleCompo/ContactTitle.js
+++ src/Components/Contacts/ContactTitleCompo/ContactTitle.js
@@ -3,9 +3,7 @@ import { Col } from 'react-bootstrap';
 import './ContactTitle.scss'
 function ContactTitle(props) {
     return (
-        <Col className="no-padding col-12">
-            <h1 id="pageTitle">{props.title}</h1>
-        </Col>
+        <h1 id="pageTitle">{props.title}</h1>
     );
 }
 
