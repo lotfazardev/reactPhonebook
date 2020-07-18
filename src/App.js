06871cf4485917b345d6dee69327264faf27c4df
Mohammad H. Lotfazar <lotfazardev@users.noreply.github.com>
2020-07-18 22:05:19 +0430

    move state of ContactContainer to App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Container from 'react-bootstrap/Container';
 import LeftNav from './Components/Navbar/RightNavContainerCompo/RightNavContainer'
 import { Row, Col } from 'react-bootstrap';
 import ContactContainer from './Components/Contacts/ContactContainerCompo/ContactContainer';
 
-class App extends Component {
-  render() {
-    return (
-      <Container fluid>
-        <Row id="main-row">
-          <Col className="col-1 col-lg-2">
-            <LeftNav
-              title="پنل کاربری"
-            />
-          </Col>
-          <Col className="col-11 col-lg-10">
-            <ContactContainer/>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+function App() {
+  const [ContactData, setContactData] = useState({
+    tabelHeader: {
+      id: "#",
+      name: "نام",
+      family: "نام خانوادگی",
+      tel: "شماره تلفن",
+      note: "یادداشت ها",
+      accessibility: "دسترسی ها"
+    },
+    tabelUsers: [
+      { name: "محمدحسین", family: "لطف آذر", tel: "09038701184", note: "دانشجوی کارشناسی کامپیوتر" },
+      { name: "نادر", family: "غفوری", tel: "09010000000", note: "ارشد عمران" },
+      { name: "سیمین", family: "شکوری", tel: "09020000000", note: "دندون شهید بهشتی" },
+      { name: "احمد", family: "ذوقی", tel: "09030000000", note: "همون که خداوکیلی 100 تومن میده" }
+    ]
+  });
+  return (
+    <Container fluid>
+      <Row id="main-row">
+        <Col className="col-1 col-lg-2">
+          <LeftNav
+            title="پنل کاربری"
+          />
+        </Col>
+        <Col className="col-11 col-lg-10">
+          <ContactContainer ContactData={ContactData} />
+        </Col>
+      </Row>
+    </Container>
+  );
+
 }
 
 
--- src/Components/Contacts/ContactContainerCompo/ContactContainer.js
+++ src/Components/Contacts/ContactContainerCompo/ContactContainer.js
@@ -6,22 +6,6 @@ import ContactSreachbar from '../ContactSearchbarCompo/ContactSearchbar';
 import ContactTabel from '../ContactTabelCompo/ContactTabel';
 
 function ContactContainer(props) {
-    const [ContactData, setContactData] = useState({
-        tabelHeader:{
-            id:"#",
-            name:"نام",
-            family:"نام خانوادگی",
-            tel:"شماره تلفن",
-            note:"یادداشت ها",
-            accessibility:"دسترسی ها"
-        },
-        tabelUsers:[
-            {name:"محمدحسین",family:"لطف آذر",tel:"09038701184",note:"دانشجوی کارشناسی کامپیوتر"},
-            {name:"نادر",family:"غفوری",tel:"09010000000",note:"ارشد عمران"},
-            {name:"سیمین",family:"شکوری",tel:"09020000000",note:"دندون شهید بهشتی"},
-            {name:"احمد",family:"ذوقی",tel:"09030000000",note:"همون که خداوکیلی 100 تومن میده"}
-        ]
-    });
     return (
         <>
             <Col className="no-padding col-12 d-flex justify-content-between align-items-center mt-1">
@@ -29,7 +13,7 @@ function ContactContainer(props) {
                 <ContactSreachbar/>
             </Col>
             <Col className="no-padding col-12 mt-3">
-                <ContactTabel header={ContactData.tabelHeader} users={ContactData.tabelUsers}/>
+                <ContactTabel header={props.ContactData.tabelHeader} users={props.ContactData.tabelUsers}/>
             </Col>
         </>
     );
