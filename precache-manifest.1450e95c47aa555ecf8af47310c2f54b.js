2ab932e7d98c11a779b89c8534dafe0460208dad
Mohammad H. Lotfazar <lotfazardev@users.noreply.github.com>
2020-07-21 16:50:57 +0430

    Updates

---
+++ precache-manifest.1450e95c47aa555ecf8af47310c2f54b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d138ca2cf2ab52c8cc703f0c7d091e0c",
+    "url": "/reactPhonebook/index.html"
+  },
+  {
+    "revision": "11f9d1c2931803425bf3",
+    "url": "/reactPhonebook/static/css/main.12d2b85b.chunk.css"
+  },
+  {
+    "revision": "aac4df812c05cbb168be",
+    "url": "/reactPhonebook/static/js/2.0837a62b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reactPhonebook/static/js/2.0837a62b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11f9d1c2931803425bf3",
+    "url": "/reactPhonebook/static/js/main.e1f67e91.chunk.js"
+  },
+  {
+    "revision": "c5dd8974e5b877515840",
+    "url": "/reactPhonebook/static/js/runtime-main.d7d27da9.js"
+  },
+  {
+    "revision": "02c15255952b5087a2f620f0c235ac94",
+    "url": "/reactPhonebook/static/media/icomoon.02c15255.woff"
+  },
+  {
+    "revision": "866aa6a0e9d773948f4729983fe9fb8c",
+    "url": "/reactPhonebook/static/media/icomoon.866aa6a0.ttf"
+  },
+  {
+    "revision": "a11d0f7565a09ca375dca34e715cb172",
+    "url": "/reactPhonebook/static/media/icomoon.a11d0f75.eot"
+  },
+  {
+    "revision": "c3da3021d65c1bc422155928603b9a4d",
+    "url": "/reactPhonebook/static/media/icomoon.c3da3021.svg"
+  }
+]);
